c62e5b706df94192ecaac7526ab6cd1caf8360b4
Thanasis Petsinas <petfountas@hotmail.com>
2019-05-02 18:29:13 +0300

    Updated bootstrap theme

--- searcHestia/searcHestia/App_Start/BundleConfig.cs
+++ searcHestia/searcHestia/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace searcHestia
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-new-theme.css",
                       "~/Content/site.css"));
         }
     }
